85e43028db8896a4e7db21c167216513e9703201
jo-rdan <jordankayinamura@gmail.com>
2020-09-19 22:13:13 +0200

    add eslint scripts

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.use("/", (req, res) =>
-  res
-    .status(200)
-    .json({ status: 200, message: "Welcome to Wake N' Bake delivery app!" })
-);
+app.use('/', (req, res) => res
+  .status(200)
+  .json({ status: 200, message: "Welcome to Wake N' Bake delivery app!" }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/models/schemas/users.js
+++ src/models/schemas/users.js
@@ -1,7 +1,7 @@
-'use strict';
 const {
   Model
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class users extends Model {
     /**
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
+  }
   users.init({
     userFirstName: DataTypes.STRING,
     userLastName: DataTypes.STRING,
