1326f7115a2993f5f5b4476f1848f10962fa6a31
jo-rdan <jordankayinamura@gmail.com>
2020-10-06 22:41:08 +0200

    ch(heroku): add heroku procfile [Delivers #175109674]

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import routes from './routes/index';
+import express from "express";
+import bodyParser from "body-parser";
+import routes from "./routes/index";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use('/', (req, res) => res
-//   .status(200)
-//   .json({ status: 200, message: "Welcome to Wake N' Bake delivery app!" }));
-
-app.use('/api/V1/', routes);
+app.use("/api/V1/", routes);
+app.use("/", (req, res) =>
+  res
+    .status(200)
+    .json({ status: 200, message: "Welcome to Wake N' Bake delivery app!" })
+);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`App is listening on port ${PORT}`));
